a880b0189d9ac339d014574886352183441037f2
Thulani-Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-02-16 19:43:07 +0200

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 * main - Entry point*
 * Return: Always 0 (Success)
@@ -6,8 +7,8 @@
 
 int main(void)
 {
-  char sentence[] = "\"Programming is like building a multilingual puzzle";
-  puts(sentence);
+char sentence[] = "\"Programming is like building a multilingual puzzle";
+puts(sentence);
 
- return (0);
+return (0);
 }
