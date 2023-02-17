4fdacb3a07e678b9febefd7301c93c6ece1f1067
Thulani-Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-02-17 15:17:03 +0200

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Code prints the alphabet in lowercase
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char alphabet = 'a';
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+		putchar('\n');
+	}
+	return (0);
+}
