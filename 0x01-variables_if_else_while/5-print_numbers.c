983bbadc58c76ccab6b83b49d3b63de79dc1bda7
Thulani-Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-02-17 16:10:15 +0200

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - prints single digits from 0
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
