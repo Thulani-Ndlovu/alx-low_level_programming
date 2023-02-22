06e282daa6b0e9dfb297b9db4db47a8ec39b1f0a
Thulani-Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-02-22 02:11:17 +0200

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,17 +13,28 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if ((i * j) < 10)
-			_putchar((i * j) + '0');
-			else if ( (i * j) >= 10)
+			if ((i * j) < 10 && j != 9)
+			{
+				_putchar(' ');
+				_putchar((i * j) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if ((i * j) >= 10 && j != 9)
+			{
+				_putchar(((i * j) / 10) + '0');
+				_putchar(((i * j) % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if ((i * j) >= 10 && j == 9)
 			{
 				_putchar(((i * j) / 10) + '0');
 				_putchar(((i * j) % 10) + '0');
 			}
-			if (j != 9)
+			else if ((i * j) < 10 && j == 9)
 			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar((i * j) + '0');
 			}
 		}
 		_putchar('\n');
