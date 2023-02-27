8cad8083ff9d0c7553f004fca7ba10fec5f7a440
Thulani-Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-02-27 23:45:35 +0200

    C

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - generates a random valid password
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+	srand(time(NULL));
+	char digits[10] = "0123456789";
+	char small_letters[26] = "abcdefghijklmnopqrstuvwxyz";
+	char upper_case[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char characters[17] = "!@#$%^&*+_-?><|/";
+	int i = 0;
+	char password[8];
+
+	while (i < 8)
+	{
+		if (i == 0 || i == 7)
+			password[i] = upper_case[(rand() % 26)];
+		else if (i == 1 || i == 6)
+			password[i] = small_letters[(rand() % 26)];
+		else if (i == 2 || i == 5)
+			password[i] = digits[(rand() % 10)];
+		else
+			password[i] = characters[(rand() % 17)];
+
+		i++;
+	}
+
+	printf("%s\n",password);
+
+	return (0);
+}
