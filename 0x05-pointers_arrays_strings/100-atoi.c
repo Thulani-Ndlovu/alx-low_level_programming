887f5f2b58ed3d40d0f4040208c4b1fded595178
Thulani-Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-02-28 00:19:32 +0200

    Task 10: Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _atoi - converts a string to an integer
+* @s: holds a string
+* Return: an integer
+*/
+
+int _atoi(char *s)
+{
+	unsigned int answer = 0;
+	int polarity = 1;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			polarity *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+			answer = (answer * 10) + (s[i] - '0');
+		else if (answer > 0)
+			break;
+		i++;
+	}
+
+	return (answer * polarity);
+}
