fc1a0afa77cc507ffad5ca0577114ce060594302
Thulani-Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-02-27 16:04:45 +0200

    Task 6: Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* puts2 - prints every other character in a string from first character.
+* @str: holds a string
+*/
+
+void puts2(char *str)
+{
+	int len = 0;
+	int i;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
