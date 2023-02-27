f6ea96bf5c51c2ff0e54a391bbf35bfd5b54adb0
Thulani-Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-02-28 01:18:32 +0200

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,30 +9,27 @@
 
 int main(void)
 {
-	char digits[10] = "0123456789";
-	char small_letters[26] = "abcdefghijklmnopqrstuvwxyz";
-	char upper_case[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char characters[17] = "!@#$%^&*+_-?><|/";
+	int n = 0;
+	int sum = 0;
 	int i = 0;
-	char password[8];
+	int password[8];
 
 	srand(time(NULL));
 
-	while (i < 8)
+	while (i < 100)
 	{
-		if (i == 0 || i == 7)
-			password[i] = upper_case[(rand() % 26)];
-		else if (i == 1 || i == 6)
-			password[i] = small_letters[(rand() % 26)];
-		else if (i == 2 || i == 5)
-			password[i] = digits[(rand() % 10)];
-		else
-			password[i] = characters[(rand() % 17)];
+		password[i] = rand() % 78;
+		sum = sum + (password[i] + '0');
+		printf("%d", password[i]);
 
-		i++;
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum = sum + n;
+			printf("%d", n);
+			break;
+		}
 	}
-
-	printf("%s\n",password);
 
 	return (0);
 }
