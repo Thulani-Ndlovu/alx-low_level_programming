7e87e77c1862bc4809c8e57469ebf7adc197e3fe
Thulani-Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-03-02 00:54:20 +0200

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strcmp - compares two strings
+* @s1: first string
+* @s2: second string
+* Return: 0 if strings are equal, > 0 or < 0 if not equal
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int len_s1;
+	int len_s2;
+	unsigned int tally = 0;
+	int i;
+
+	len_s1 = strlen(s1);
+	len_s2 = strlen(s2);
+
+	if (len_s1 <= len_s2)
+	{
+		for (i = 0; i < len_s1; i++)
+		{
+			if (s1[i] != s2[i])
+			{
+				tally = (s1[i] - '0') - (s2[i] - '0');
+				break;
+			}
+		}
+	}
+
+	else
+	{
+		for (i = 0; i < len_s2; i++)
+		{
+			if (s1[i] != s2[i])
+			{
+				tally = (s1[i] - '0') - (s2[i] - '0');
+				break;
+			}
+		}
+	}
+
+	return (tally);
+}
