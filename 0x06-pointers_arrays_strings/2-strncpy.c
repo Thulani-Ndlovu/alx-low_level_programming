eeeee7613cc212694141bb16b66f5ce4016268d9
Thulani-Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-03-01 21:22:25 +0200

    Task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,21 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+	int len_src = 0;
+
+	i = 0;
+
+	while (src[i++])
+		len_src++;
 
 	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
 
+	for (i = len_src; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
 	return (dest);
 }
