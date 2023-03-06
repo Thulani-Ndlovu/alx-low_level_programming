6b93ce818aa23f7ff857431347c689838e8215d1
Thulani-Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-03-06 18:30:47 +0200

    Task 2: strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _strchr - locates a character in a string
+* @s: pointer to a string
+* @c: character being searched
+* Return: returns a pointer to the first occurrence of c
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+
+	return (NULL);
+}
