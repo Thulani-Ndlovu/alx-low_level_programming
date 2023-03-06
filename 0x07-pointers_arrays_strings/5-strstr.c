405e33b69ae888073329bf6e45da1ed7cce795f6
Thulani-Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-03-07 01:50:48 +0200

    Task 6

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _strstr - locates a substring
+* @haystack: points to the search string
+* @needle: points to the match string
+* Return: pointer to the beginning of the located substring
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+
