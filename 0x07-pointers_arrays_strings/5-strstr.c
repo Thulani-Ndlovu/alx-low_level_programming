64d158b5d043ed1069d19dfde738a59f3a2703de
Thulani-Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-03-07 02:29:10 +0200

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,4 +9,25 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+	int i;
 
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+				{
+					return (haystack);
+				}
+
+				i++;
+			} while (haystack[i] == needle[i]);
+		}
+		haystack++;
+	}
+
+	return (NULL);
+}
