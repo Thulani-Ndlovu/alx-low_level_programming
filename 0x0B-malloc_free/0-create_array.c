0b4f7c421790885f91b0eb16dd019ae298ca0a66
Thulani-Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-03-13 19:37:23 +0200

    Task 0:final attempt

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,6 +17,10 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
+
+	if (ptr == 0)
+		return (NULL);
+
 	else if (size > 0)
 	{
 		ptr = (char *) malloc(size * sizeof(char));
