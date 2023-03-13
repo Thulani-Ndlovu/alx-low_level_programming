f07d6634c470d9050df8193f1a87f81dd1f84883
Thulani-Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-03-13 07:14:59 +0200

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - prints the number of arguments passed into the program
+* @argc: argument count
+* @argv: argument vector
+* Return: Always 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
