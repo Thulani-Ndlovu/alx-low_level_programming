d6c7db6a50d99a8ff6ba6f5cc286f433ba6814b2
Thulani-Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-03-14 22:54:24 +0200

    Task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -17,7 +17,7 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	ptr = (char *) malloc(2* ac * sizeof(char));
+	ptr = (char *) malloc(2 * ac * sizeof(char));
 	for (i = 0; i < ac; i++)
 	{
 		strcat(ptr, av[i]);
