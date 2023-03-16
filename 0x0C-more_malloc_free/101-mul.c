95cddb48a40302480b225ea94e146552517aca76
Thulani-Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-03-16 23:49:26 +0200

    Task 5

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* main - multiplies two positive integers
+* @ac: argument count
+* @argv: argument vector
+* Return: 0 (success)
+*/
+
+int main(int ac, char **argv)
+{
+	int i, j;
+	unsigned long mul = 0, num1 = 0, num2 = 0;
+
+	if (ac != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (i = 1; i < ac; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 58 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	num1 = atol(argv[1]);
+	num2 = atol(argv[2]);
+	mul = num1 * num2;
+	printf("%lu\n", mul);
+
+	return (0);
+}
