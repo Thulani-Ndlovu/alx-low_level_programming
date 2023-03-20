01dfc2cbc994849c796b8653b495989b18c55f87
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-03-20 16:53:28 +0200

    Task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,6 +12,8 @@ void print_dog(struct dog *d)
 		d->name = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
+	if (d == NULL)
+		return;
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
