12e21d77ad992d753da06f0daa55d3099d4b7e5e
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-03-20 17:44:30 +0200

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,39 @@
 #include <stdlib.h>
 
 /**
+* _strlen - length of a string
+* @str: string
+* Return: length of string
+*/
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+* _strcpy - copy a string
+* @dest: destination string
+* @src: source string
+* Return: copied string
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
 * new_dog - creates a new dog
 * @name: name of the dog
 * @age: age of the dog
@@ -13,13 +46,35 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *info;
 
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
 	info = malloc(sizeof(dog_t));
 
 	if (info == NULL)
 		return (NULL);
-	info->name = name;
+
+
+	info->name = malloc(sizeof(char) * (_strlen(name) + 1));
+
+	if (info->name == NULL)
+	{
+		free(name);
+		return (NULL);
+	}
+
+	info->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+
+	if (info->owner == NULL)
+	{
+		free(info->name);
+		free(info);
+		return (NULL);
+	}
+
+	info->name = _strcpy(info->name, name);
 	info->age = age;
-	info->owner = owner;
+	info->owner = _strcpy(info->owner, owner);
 
 	return (info);
 }
