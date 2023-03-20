a15d417fd440751d0df2ca5c3fe8a113a8513c5e
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-03-20 14:52:50 +0200

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - structure that holds the info about the dog
+* @name: name of the dog
+* @age: age of the dog
+*
+* Description: it contains the name, age and the onwer of the dog.
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	/**
+	  * @owner: owner of the dog
+	  *
+	  * Description: information about the dog and its ownership
+	  */
+	char *owner;
+};
+
+#endif
