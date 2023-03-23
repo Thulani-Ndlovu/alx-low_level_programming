6088b3adac53a502b0b3b6520c93ef439248693f
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-03-23 19:40:12 +0200

    Task 2: One woman's constant is another woman's variable

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,8 +21,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", s);
 		else
 			printf("(nil)");
-		if (i < (n - 1) && separator != NULL)
-			printf("%s", separator);
+		if (i < (n - 1))
+			if (separator)
+				printf("%s", separator);
 	}
 	printf("\n");
 	va_end(ap);
