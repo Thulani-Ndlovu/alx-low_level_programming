c3ac0fc309872490814ede5494c992fdd7ef7ae2
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-04-04 14:59:30 +0200

    Task 7: get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,13 +11,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (head == NULL)
-		return (NULL);
-
 	for (i = 0; i < index; i++)
 	{
-		if (i == index)
-			break;
+		if (head == NULL)
+			return (NULL);
 		head = head->next;
 	}
 
