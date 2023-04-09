81bb4f85909708a68bf856d0910c40aa6853c1a8
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-04-09 23:06:44 +0200

    Task 3: Set the value of abit to 1 at a given index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* set_bit - sets the value of a bit to 1 at a given index
+* @n: decimal number
+* @index: index of the bit we want to set
+* Return: 1 (if it worked), otherwise -1
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int long bit_memory;
+
+	bit_memory = sizeof(unsigned int long) * 8;
+
+	if (index >= bit_memory)
+		return (-1);
+
+	*n = *n ^ (1 << index);
+	return (1);
+}
