436dcf90573c2b0316013112a00a0bf5f1332b2d
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-04-10 16:38:27 +0200

    Task 0: Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* read_textfile - Reads a text file and prints it to the POSIX
+* Standard Output
+* @filename: the file to read from
+* @letters: number of letters the function should read and print
+* Return: number of letters that were read and printed.
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *text;
+	ssize_t WRITE, READ, OPEN;
+
+	if (filename == NULL)
+		return (0);
+
+	text = malloc(sizeof(char *));
+
+	if (text == NULL)
+		return (0);
+
+	OPEN = open(filename, O_RDONLY);
+	READ = read(OPEN, text, letters);
+	WRITE = write(STDOUT_FILENO, text, READ);
+
+	if (WRITE == -1 || READ == -1 || OPEN == -1 || WRITE != READ)
+	{
+		free(text);
+		return (0);
+	}
+
+	free(text);
+
+	return (WRITE);
+}
