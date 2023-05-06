971e5c95e1d1a0da55aa9037fd26669751dba1a5
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-05-06 15:51:47 +0200

    Task 6. Endianness: Function that checks the endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* get_endianness - checks the endianness
+* Return: 0 if big endian, otherwise 1
+*/
+
+int get_endianness(void)
+{
+	int number;
+	char *endian;
+
+	number = 1;
+	endian = (char *)&number;
+
+	if (*endian == 1)
+		return (1);
+	return (0);
+}
